f24114bf7af3583bfae50dfac627ed431d5c6306
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-09-20 02:19:23 +0100

    Expanded TUI
    todo: refactor

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 
 import XInput as xi
+import sys
 import time
 
 
-def main():
-    print("Hello from joymon!")
 
-    print("connected controllers:")
-    for i, s in enumerate(xi.get_connected()):
-        print(f'{i}: {s}')
     
-    index = int(input("pick a controller: "))
+def slowprint(*args, d=0.05) -> None:
+    time.sleep(d)
+    print(*args)
+
+
+def main() -> None:
+    slowprint("Hello from joymon!")
+
+    detected: list[int] = []
+    selected: int | None = None
+
+    while selected is None:
+        slowprint("\nconnected controllers:\n")
+        for i, s in enumerate(xi.get_connected()):
+            if s:
+                detected.append(i)
+                slowprint(f'    {i}: {s}')
+                
+        slowprint('\nenter "q" to exit')
+        command = input("pick a controller: ")
+        
+        if command == "q":
+            sys.exit()
+        if not command.isnumeric():
+            slowprint(f'\n -- ERROR: invalid command "{command}"')
+            continue
+            
+        index = int(command)
+        
+        if index in detected:
+            selected = index
+        else:
+            slowprint(f"\n -- ERROR: controller {index} not found. Please try again.")
     
     while True:
-        time.sleep(0.1)
-        print(xi.get_trigger_values(xi.get_state(index))[0])
+        slowprint(xi.get_trigger_values(xi.get_state(index))[0], d=0.1)
     
 
 

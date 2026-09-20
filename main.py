196b94e83df3ea9d5d9b8d3d9715886169ac29de
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-09-20 01:40:48 +0100

    Made a simple TUI

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+
+import XInput as xi
+import time
+
+
 def main():
     print("Hello from joymon!")
+
+    print("connected controllers:")
+    for i, s in enumerate(xi.get_connected()):
+        print(f'{i}: {s}')
+    
+    index = int(input("pick a controller: "))
+    
+    while True:
+        time.sleep(0.1)
+        print(xi.get_trigger_values(xi.get_state(index))[0])
+    
 
 
 if __name__ == "__main__":
